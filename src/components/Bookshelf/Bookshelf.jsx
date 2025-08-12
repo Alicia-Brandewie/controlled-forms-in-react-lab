07f1db2ce31ab3c9140ea588496cf527b2bf0961
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-08-12 14:16:51 -0500

    one and two complete, working on three

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,5 +1,28 @@
 import { useState } from "react";
 
+const [books, setBooks] = useState([])
+
+const [newBook, setNewBook] = useState([
+  { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+  { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+])
+
+
+const handleInputChange = (eventInput) => {
+  setBookInput(eventInput.target.value);
+}
+
+
+
+
+
+
+
+const handleSubmit = (eventInput) => {
+  setBookInput(eventInput.target.value);
+}
+
+
 
 <div className="bookshelfDiv">
   <div className="formDiv">
@@ -8,3 +31,34 @@ import { useState } from "react";
   </div>
   <div className="bookCardsDiv">{/* Book cards will display here */}</div>
 </div>
+
+export default Bookshelf
+
+
+// ______________________________________
+return (
+  <>
+    <h1> Add a new book!</h1>
+    <form>
+      <label htmlFor="title">Title:</label>
+      <input
+        type="text"
+        id="title"
+        name="title"
+        value={title}
+        onChange={handleInputChange}
+      />
+
+      <label htmlFor="author">Author:</label>
+      <input
+        type="text"
+        id="author"
+        name="author"
+        value={author}
+        onChange={handleInputChange}
+      />
+
+    </form>
+  </>
+);
+export default Form 
