13ea865766f56da9eb42c523e65c7abe1aa51388
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-08-13 16:34:40 -0500

    three and four done

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,51 +1,41 @@
 import { useState } from "react";
 
+const Bookshelf = () => {
+
 const [books, setBooks] = useState([])
 
 const [newBook, setNewBook] = useState([
-  { title: 'Fourth Wing', author: 'Rebecca Yarros' },
-  { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+  { title: '', author: ''},
+  // { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+  // { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
 ])
 
 
 const handleInputChange = (eventInput) => {
-  setBookInput(eventInput.target.value);
-}
-
-
-
-
-
-
+  // setBookInput(eventInput.target.value);
+  setNewBook({ ...newBook, [eventInput.target.name]: eventInput.target.value })
+};
 
 const handleSubmit = (eventInput) => {
-  setBookInput(eventInput.target.value);
-}
+  // setBookInput(eventInput.target.value);
+  eventInput.preventDefault();
+  setNewBook({title: '', author: ''});
+};
 
 
-
+return (
+  <>
 <div className="bookshelfDiv">
   <div className="formDiv">
     <h3>Add a Book</h3>
-    {/* Form will go here */}
-  </div>
-  <div className="bookCardsDiv">{/* Book cards will display here */}</div>
-</div>
 
-export default Bookshelf
-
-
-// ______________________________________
-return (
-  <>
-    <h1> Add a new book!</h1>
-    <form>
+<form onSubmit={handleSubmit}>
       <label htmlFor="title">Title:</label>
       <input
         type="text"
         id="title"
         name="title"
-        value={title}
+        value={newBook.title}
         onChange={handleInputChange}
       />
 
@@ -54,11 +44,21 @@ return (
         type="text"
         id="author"
         name="author"
-        value={author}
+        value={newBook.author}
         onChange={handleInputChange}
       />
-
+    <button type="submit">Submit a book</button>
     </form>
+
+
+
+  </div>
+  <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+</div>
   </>
 );
-export default Form 
+}
+
+export default Bookshelf
+
+
